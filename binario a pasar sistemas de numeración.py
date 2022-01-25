03bf896e965c6dbb1e280ba2c63436c06af7c4e4
osoa9manuelmunoz <80717541+osoa9manuelmunoz@users.noreply.github.com>
2022-01-24 21:48:21 -0500

    Update binario a pasar sistemas de numeración.py

--- binario a pasar sistemas de numeración.py
+++ binario a pasar sistemas de numeración.py
@@ -1,25 +1,51 @@
-def convertir_base(numero,base): 
-    conversion = "6789"  
+n = int(input("para calcular un numero de base 10 a otro base precione (1) o si quieres alcular otro numero a base 10 preciones(2): "))
 
-    if numero < base:              
-        return conversion[numero]  
+if n==1:
+    numero=int(input("ingrese el numero que esta en base 10: "))
+    base=int(input("Ingrese base que le deseas remplazar : "))
     
-    else:                                                                     
-        return convertir_base(numero//base, base) + conversion[numero % base] 
+    def imprime_digito(digito):
+        if digito < 10:
+            print("{:d}".format(digito), end="")
+        else:
+            digito_t = chr(digito - 10 + ord('A'))
+            print("{}".format(digito_t), end="")
+        
+    def imprime_otra_base(numero, base):
+        if numero > 0:
+            digito = numero % base
+            numero //= base
+            imprime_otra_base(numero, base)
+            imprime_digito(digito)
 
-numero = int(input("ingrese numero base 10:  "))
-base = int(input("ingrese base que desea calcular del numero en base 10: "))
-resultado = convertir_base(numero,base)
-print("el resultado de la base deseado es: ",resultado)      
-
-def binario_a_decimal(numero_binario):
-	numero_decimal = 0 
-
-	for posicion, digito_string in enumerate(numero_binario[::-1]):
-		numero_decimal += int(digito_string) * 2 ** posicion
-
-	return numero_decimal
-
-print("el numero a binario es: ", binario_a_decimal(convertir_base(numero,base)))
+    if base >= 2 and base <= 36:
+        imprime_otra_base(numero, base)
 
 
+    else:
+        print("Debe ingresar una base entre 2 y 36")
+
+
+elif n==2:
+    numero = input("ingrese el numero para pasarlo a base 10: ")
+    base = int(input("ingrese la base del numero: "))
+    base10 = 0
+    a = []
+
+    tam = len(numero)
+
+    for m in range (len(numero)):
+        a.append(numero[m])
+
+    c = tam -1
+
+    tem = 0 
+
+    for i in range(tam):
+        tem = int(a[i]) * pow(base,c)
+        c = c-1
+        base10 = base10 + tem
+    print("le numero a numero decimal es: {} ".format(base10))
+
+else:
+    print("porfavor ingrese alguno de la seleccion ")
